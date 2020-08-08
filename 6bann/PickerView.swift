deeab6f33c79e5d601d3bfc2449e98aaec6369fd
naru <mibo55rui@gmail.com>
2020-08-08 11:26:09 +0900

    n

---
+++ 6bann/PickerView.swift
@@ -0,0 +1,31 @@
+//
+//  PickerView.swift
+//  6bann
+//
+//  Created by 橋目　那桜 on 2020/08/08.
+//  Copyright © 2020 橋目　那桜. All rights reserved.
+//
+
+import UIKit
+
+class PickerView: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
